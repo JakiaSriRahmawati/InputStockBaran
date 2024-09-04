da3e6959b5a81a20391846f6750520f290dbcb02
JakiaSriRahmawati <jakiasrirahmawati@gmail.com>
2024-09-04 08:39:38 +0700

    first commit

---
+++ project_InputStockBarang_zakia/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace project_InputStockBarang_zakia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtKodebarang = new System.Windows.Forms.TextBox();
+            this.txtNamabarang = new System.Windows.Forms.TextBox();
+            this.txtHargabarang = new System.Windows.Forms.TextBox();
+            this.txtJumlahbarang = new System.Windows.Forms.TextBox();
+            this.txtSatuan = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnsimpn = new System.Windows.Forms.Button();
+            this.btnbatl = new System.Windows.Forms.Button();
+            this.btnhaps = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kode Barang :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(75, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Barang :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Harga Barang :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(75, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jumlah Barang :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(75, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Satuan :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.WindowFrame;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(611, 165);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // txtKodebarang
+            // 
+            this.txtKodebarang.Location = new System.Drawing.Point(245, 66);
+            this.txtKodebarang.Name = "txtKodebarang";
+            this.txtKodebarang.Size = new System.Drawing.Size(204, 20);
+            this.txtKodebarang.TabIndex = 6;
+            this.txtKodebarang.TextChanged += new System.EventHandler(this.txtKodebarang_TextChanged);
+            // 
+            // txtNamabarang
+            // 
+            this.txtNamabarang.Location = new System.Drawing.Point(245, 93);
+            this.txtNamabarang.Name = "txtNamabarang";
+            this.txtNamabarang.Size = new System.Drawing.Size(204, 20);
+            this.txtNamabarang.TabIndex = 7;
+            // 
+            // txtHargabarang
+            // 
+            this.txtHargabarang.Location = new System.Drawing.Point(245, 119);
+            this.txtHargabarang.Name = "txtHargabarang";
+            this.txtHargabarang.Size = new System.Drawing.Size(204, 20);
+            this.txtHargabarang.TabIndex = 8;
+            // 
+            // txtJumlahbarang
+            // 
+            this.txtJumlahbarang.Location = new System.Drawing.Point(245, 146);
+            this.txtJumlahbarang.Name = "txtJumlahbarang";
+            this.txtJumlahbarang.Size = new System.Drawing.Size(204, 20);
+            this.txtJumlahbarang.TabIndex = 9;
+            // 
+            // txtSatuan
+            // 
+            this.txtSatuan.Location = new System.Drawing.Point(245, 172);
+            this.txtSatuan.Name = "txtSatuan";
+            this.txtSatuan.Size = new System.Drawing.Size(204, 20);
+            this.txtSatuan.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(645, 192);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tampil Data";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(200, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(283, 28);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Memasukkan Stock Barang";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(478, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 17);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Simpan";
+            this.label7.Click += new System.EventHandler(this.Label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(584, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 17);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Batal";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(534, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 17);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Hapus";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // btnsimpn
+            // 
+            this.btnsimpn.BackgroundImage = global::project_InputStockBarang_zakia.Properties.Resources.pngtree_save_as_icon_png_image_4546002__1__removebg_preview;
+            this.btnsimpn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnsimpn.FlatAppearance.BorderSize = 0;
+            this.btnsimpn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsimpn.Location = new System.Drawing.Point(481, 144);
+            this.btnsimpn.Name = "btnsimpn";
+            this.btnsimpn.Size = new System.Drawing.Size(47, 36);
+            this.btnsimpn.TabIndex = 24;
+            this.btnsimpn.UseVisualStyleBackColor = true;
+            this.btnsimpn.Click += new System.EventHandler(this.btnsimpn_Click);
+            // 
+            // btnbatl
+            // 
+            this.btnbatl.BackgroundImage = global::project_InputStockBarang_zakia.Properties.Resources.images__1___1__removebg_preview__1___1_;
+            this.btnbatl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnbatl.FlatAppearance.BorderSize = 0;
+            this.btnbatl.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbatl.Location = new System.Drawing.Point(537, 144);
+            this.btnbatl.Name = "btnbatl";
+            this.btnbatl.Size = new System.Drawing.Size(41, 36);
+            this.btnbatl.TabIndex = 25;
+            this.btnbatl.UseVisualStyleBackColor = true;
+            this.btnbatl.Click += new System.EventHandler(this.btnbatl_Click);
+            // 
+            // btnhaps
+            // 
+            this.btnhaps.BackgroundImage = global::project_InputStockBarang_zakia.Properties.Resources.download__2___1___1__removebg_preview;
+            this.btnhaps.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnhaps.FlatAppearance.BorderSize = 0;
+            this.btnhaps.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnhaps.Location = new System.Drawing.Point(584, 144);
+            this.btnhaps.Name = "btnhaps";
+            this.btnhaps.Size = new System.Drawing.Size(37, 36);
+            this.btnhaps.TabIndex = 26;
+            this.btnhaps.UseVisualStyleBackColor = true;
+            this.btnhaps.Click += new System.EventHandler(this.btnhaps_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(691, 426);
+            this.Controls.Add(this.btnhaps);
+            this.Controls.Add(this.btnbatl);
+            this.Controls.Add(this.btnsimpn);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSatuan);
+            this.Controls.Add(this.txtJumlahbarang);
+            this.Controls.Add(this.txtHargabarang);
+            this.Controls.Add(this.txtNamabarang);
+            this.Controls.Add(this.txtKodebarang);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "                                                                                 " +
+    "                        ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtKodebarang;
+        private System.Windows.Forms.TextBox txtNamabarang;
+        private System.Windows.Forms.TextBox txtHargabarang;
+        private System.Windows.Forms.TextBox txtJumlahbarang;
+        private System.Windows.Forms.TextBox txtSatuan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnsimpn;
+        private System.Windows.Forms.Button btnbatl;
+        private System.Windows.Forms.Button btnhaps;
+    }
+}
+
---
+++ project_InputStockBarang_zakia/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace project_InputStockBarang_zakia
+{
+    public partial class Form1 : Form
+    {
+        string database = ("server=localhost; uid=root; database=db_barang1; pwd='';");
+        public MySqlConnection koneksi;
+        public MySqlCommand cmd;
+        public MySqlDataAdapter adp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void konek()
+        {
+            koneksi = new MySqlConnection(database);
+            koneksi.Open();
+        }
+        public void disconek()
+        {
+            koneksi = new MySqlConnection(database);
+            koneksi.Close();
+        }
+        public DataTable TampilData()
+        {
+            string sql = "SELECT * FROM barang";
+            DataTable dt = new DataTable();
+            try
+            {
+                konek();
+                cmd = new MySqlCommand(sql,koneksi);
+                adp = new MySqlDataAdapter(cmd);
+                cmd.ExecuteNonQuery();
+                adp.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            disconek();
+            return dt;
+        }
+        
+        public void Query(string strQuery)
+        {
+            koneksi = new MySqlConnection(database);
+            try
+            {
+                koneksi.Open();
+                cmd = new MySqlCommand(strQuery, koneksi);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Data Berhasil di Proses");
+                koneksi.Close();
+            }
+        }
+        void Simpan()
+        {
+            Query("INSERT INTO barang VALUES('','" + this.txtKodebarang.Text +
+                "','" + this.txtNamabarang.Text + "','" + this.txtHargabarang.Text +
+                "','" + this.txtJumlahbarang.Text + "','" + this.txtSatuan.Text +
+                "')");
+        }
+        void Hapus()
+        {
+            try
+            {
+                koneksi = new MySqlConnection(database);
+                koneksi.Open();
+                string strQuery = "DELETE FROM barang WHERE id_barang ='" + idBarang+"'";
+                cmd = new MySqlCommand(strQuery, koneksi);
+                cmd.ExecuteNonQuery();
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil di Hapus");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void Batal()
+        {
+            txtKodebarang.ResetText();
+            txtNamabarang.ResetText();
+            txtHargabarang.ResetText();
+            txtJumlahbarang.ResetText();
+            txtSatuan.ResetText();
+        }
+        void Ubah()
+        {
+            try
+            {
+                koneksi = new MySqlConnection(database);
+                koneksi.Open();
+                string update = "UPDATE barang SET kode_barang=" + txtKodebarang.Text +
+                "',nama_barang ='" + txtNamabarang.Text + "',harga_barang ='" + txtHargabarang.Text +
+                "',jumlah_barang='" + txtJumlahbarang.Text + "',satuan='" + txtSatuan.Text +
+                "'WHERE idBarang='" + idBarang + "'";
+                cmd = new MySqlCommand(update, koneksi);
+                cmd.ExecuteNonQuery();
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil di Ubah");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtKodebarang.Focus();
+            TampilData();
+        }
+
+        private void txtKodebarang_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            
+        }
+        string idBarang = "";
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >=0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                idBarang = row.Cells["id_barang"].Value.ToString();
+                txtKodebarang.Text = row.Cells["kode_barang"].Value.ToString();
+                txtNamabarang.Text = row.Cells["nama_barang"].Value.ToString();
+                txtHargabarang.Text = row.Cells["harga_barang"].Value.ToString();
+                txtJumlahbarang.Text = row.Cells["jumlah_barang"].Value.ToString();
+                txtSatuan.Text = row.Cells["satuan"].Value.ToString();
+                //btnsimpn.Text = "Ubah";
+            }
+        }
+
+        private void btnBatal_Click(object sender, EventArgs e)
+        {
+            //keSemula();
+        }
+        void keSemula()
+        {
+            txtKodebarang.ResetText();
+            txtNamabarang.ResetText();
+            txtHargabarang.ResetText();
+            txtJumlahbarang.ResetText();
+            txtSatuan.Clear();
+            btnsimpn.Text = "";
+            TampilData();
+            txtKodebarang.Focus();
+        }
+
+        private void BtnBatal_Click_1(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void BtnLihat_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsimpn_Click(object sender, EventArgs e)
+        {
+            if (btnsimpn.Text == "")
+            {
+                Simpan();
+                TampilData();
+            }
+            //else if (btnsimpn.Text == "Ubah")
+            {
+                //Ubah();
+                btnsimpn.Text = "";
+                TampilData();
+            }
+        }
+
+        private void btnbatl_Click(object sender, EventArgs e)
+        {
+            DialogResult di = MessageBox.Show("Yakin mau di Hapus??", "Informasi", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+            if (di == DialogResult.No)
+            {
+                TampilData();
+            }
+            else
+            {
+                Hapus();
+                TampilData();
+            }
+        }
+
+        private void btnhaps_Click(object sender, EventArgs e)
+        {
+            keSemula();
+        }
+    }
+}
